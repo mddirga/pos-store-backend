97dbf13404e29117556ad76ab4122e975428ac35
madedirga22 <madedirga22@gmail.com>
2023-12-26 21:35:37 +0800

    Auth with Fortify and User CRUD Management

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -1,10 +1,10 @@
 <div class="main-sidebar sidebar-style-2">
     <aside id="sidebar-wrapper">
         <div class="sidebar-brand">
-            <a href="index.html">POS - STORE</a>
+            <a href="home">POS - STORE</a>
         </div>
         <div class="sidebar-brand sidebar-brand-sm">
-            <a href="index.html">PS</a>
+            <a href="home">PS</a>
         </div>
         <ul class="sidebar-menu">
             <li class="menu-header">Dashboard</li>
@@ -20,6 +20,17 @@
                         <a class="nav-link"
                             href="{{ url('dashboard-ecommerce-dashboard') }}">Ecommerce Dashboard</a>
                     </li>
+                </ul>
+            </li>
+            <li class="nav-item dropdown">
+                <a href="#"
+                    class="nav-link has-dropdown"><i class="fas fa-fire"></i><span>Users</span></a>
+                <ul class="dropdown-menu">
+                    <li >
+                        <a class="nav-link"
+                            href="{{ route('user.index') }}">All Users</a>
+                    </li>
+                    
                 </ul>
             </li>
     </aside>
